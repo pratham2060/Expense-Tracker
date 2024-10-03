310bbdada3981d7309b92488acfcfc2347cec1ef
pratham2006 <prathameshcharoaskar@gmail.com>
2024-10-03 20:02:54 +0530

    final fixes

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -11,7 +11,15 @@ function Header() {
 
   return (
     <div className="p-6 flex items-center border justify-between shadow-lg" style={{ height: "70px" }}>
-      <Image src="/logo.svg" alt="Logo" width={60} height={50} />
+      <div className="flex items-center">
+        <Image src="/logo.svg" alt="Logo" width={60} height={50} />
+        <h2 className="text-xl font-extrabold">HISABH</h2>
+      </div>
+      <div className="font-bold text-lg  text-black flex items-center">
+        <Link href={'/'} className="hover:text-primary" ><h3>Home</h3></Link>
+        <Link href={'/about'} className="ml-6 hover:text-primary"><h3>About</h3></Link>
+        <Link href={'#'} className="ml-6 hover:text-primary"><h3>Contact</h3></Link>
+      </div>
       {isSignedIn ? (
         <>
           <div className="flex gap-1 justify-end">
