eff8d96c8bf842d6d3aeec95617e37077cd7b505
pratham2006 <prathameshcharoaskar@gmail.com>
2024-10-03 18:26:18 +0530

    final fixes

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -4,4 +4,6 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema'
 
 const sql = neon(process.env.NEXT_PUBLIC_DATABASE_URL);
-export const db = drizzle(sql,{schema});
+const db = drizzle(sql,{schema});
+
+export default db;
