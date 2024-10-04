6b80f1579dd2707b5c7acc691e090c3291a7f314
pratham2006 <prathameshcharoaskar@gmail.com>
2024-10-04 18:02:26 +0530

    final fixes

--- middleware.ts
+++ middleware.ts
@@ -6,35 +6,34 @@ const isPublicRoute = createRouteMatcher([
     "/sign-up",
     "/",
     "/about"
-])
-
-
+]);
 
 export default clerkMiddleware((auth, req) => {
-    const {userId} = auth();
-    const currentUrl = new URL(req.url)
-     const isAccessingDashboard = currentUrl.pathname === "/dashboard"
-     const isApiRequest = currentUrl.pathname.startsWith("/api")
+    const { userId } = auth();
+    const currentUrl = new URL(req.url);
+    const isAccessingDashboard = currentUrl.pathname === "/dashboard";
+    const isApiRequest = currentUrl.pathname.startsWith("/api");
 
-     // If user is logged in and accessing a public route but not the dashboard
-    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
-        return NextResponse.redirect(new URL("/dashboard", req.url))
+    // If user is logged in and accessing a public route but not the dashboard
+    if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
     }
-    //not logged in
-    if(!userId){
+
+    // If not logged in
+    if (!userId) {
         // If user is not logged in and trying to access a protected route
-        if(!isPublicRoute(req)){
-            return NextResponse.redirect(new URL("/sign-in", req.url))
+        if (!isPublicRoute(req)) {
+            return NextResponse.redirect(new URL("/sign-in", req.url)); // Redirect to sign-in instead of sign-up
         }
 
         // If the request is for a protected API and the user is not logged in
-        if(isApiRequest){
-            return NextResponse.redirect(new URL("/sign-in", req.url))
+        if (isApiRequest) {
+            return NextResponse.redirect(new URL("/sign-in", req.url));
         }
     }
-    return NextResponse.next()
 
-})
+    return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
