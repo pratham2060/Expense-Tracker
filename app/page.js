2ab60936df256978336b1262e19cda72d382f58f
Prathamesh Charoskar <145451768+pratham2060@users.noreply.github.com>
2024-12-08 20:55:11 +0530

    Added title to home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Header from './_components/Header';
 import Hero from './_components/Hero';
 import Footer from './_components/Footer';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className='relative'>
+    <Head> 
+      <title>Hisabh - Get ready to manage your expenses</title> 
+    </Head>
       <Header />
       <Hero />
       <Footer />
